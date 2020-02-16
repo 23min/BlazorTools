9c6f340087c41e721c530b9b6fa203795244fc98
Peter Bruinsma <Peter.Bruinsma@biometria.se>
2020-02-16 16:20:09 +0100

    Rename static files.

--- Extensions/InteropExtensions.cs
+++ Extensions/InteropExtensions.cs
@@ -7,7 +7,7 @@ namespace Proliminal.BlazorTools.Extensions
     {
         public static async Task<string> FormatJson(this IJSRuntime _JSRuntime, string json)
         {
-            return await _JSRuntime.InvokeAsync<string>("exampleJsFunctions.FormatJson", json);
+            return await _JSRuntime.InvokeAsync<string>("BlazorTools.FormatJson", json);
         }
     }
 }
